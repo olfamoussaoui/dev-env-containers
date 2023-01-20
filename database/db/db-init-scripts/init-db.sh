eabaedd05d49db5085c2d62660652fd3e4315738
olfamoussaoui <moussaoui.olfa1@gmail.com>
2023-01-21 00:26:27 +0100

    update the init-db.sh script

--- database/db/db-init-scripts/init-db.sh
+++ database/db/db-init-scripts/init-db.sh
@@ -15,5 +15,20 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
 	username character varying(45) NOT NULL UNIQUE,
 	password character varying(45) NOT NULL
 	);
+	CREATE TABLE books(
+  	id SERIAL PRIMARY KEY,
+  	name character varying(45) NOT NULL,
+  	category character varying(45) NOT NULL,
+  	author character varying(45) NOT NULL
+  	);
+  	CREATE TABLE reviews(
+      	id SERIAL PRIMARY KEY,
+      	content character varying(45) NOT NULL,
+      	stars int NOT NULL,
+      	book_id SERIAL,
+      	CONSTRAINT fk_book
+              FOREIGN KEY(book_id)
+        	  REFERENCES books(id)
+      	);
   COMMIT;
 EOSQL
